9cef8eb2335dedebd8def1b030842940e1cf654c
Aaron Raycove <aacra0820@gmail.com>
2024-03-18 16:38:14 -0600

    WOOO, the basic GUI is done, still need to plug in the update score method, and get media to display

--- api_calls.py
+++ api_calls.py
@@ -1,6 +1,53 @@
-def update_score(answer, file_name):
-    # encode filename
-    
-    # pass answer and filename to api to update score
+import requests
+import os
+import subprocess
+from urllib.parse import quote
+def launch_api():
+    project_root = os.path.dirname(os.path.abspath(__file__))  # Get path of frontend-py
+    quizzer_dir = os.path.join(project_root, "..", "quizzer")  # Navigate up two directories
+    command = f"nohup uvicorn api:app --reload"
+    subprocess.Popen(command, shell=True)
 
-    pass
+def populate_quiz():
+    root = "http://127.0.0.1:8000/"
+    command_pop_quiz = "populate_quiz"
+    data = requests.get(f"{root}{command_pop_quiz}")
+    data = data.json()
+    question_list = data["question_list"]
+    returned_sorted_questions = data["sorted_questions"]
+    return question_list, returned_sorted_questions
+def extract_question_data(current_question):
+    '''
+    Takes the current question as an argument
+    returns the relevant key:values for that question if they exist
+    '''
+    file_name = current_question.get("file_name")
+    file_path = current_question.get("file_path")
+    type = current_question.get("type")
+    subject = current_question.get("subject")
+    related = current_question.get("related")
+    question_text = current_question.get("question_text")
+    question_media = current_question.get("question_media")
+    answer_media = current_question.get("answer_media")
+    answer_text = current_question.get("answer_text")
+    next_revision_due = current_question.get("next_revision_due")    
+    revision_streak = current_question.get("revision_streak")
+    last_revised = current_question.get("last_revised")
+
+def update_score(answer, file_name):
+    encoded_file_name = quote(file_name)
+    if answer == "correct":
+        first_part = "http://127.0.0.1:8000/update_score/{status, file_name}?status=correct&file_name="
+        query = first_part + encoded_file_name
+        response = requests.get(f"{query}")
+    elif answer == "incorrect":
+        first_part = "http://127.0.0.1:8000/update_score/{status, file_name}?status=incorrect&file_name="
+        query = first_part + encoded_file_name
+        response = requests.get(f"{query}")        
+    else:
+        print("Something went wrong")
+
+def initialize_quizzer():
+    root = "http://127.0.0.1:8000/"
+    command_initialize_quizzer = "initialize_quizzer"
+    requests.get(f"{root}{command_initialize_quizzer}")
--- main.py
+++ main.py
@@ -1,43 +1,146 @@
 import os
 import requests
+import time
 from urllib.parse import quote
 
 import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty
+from api_calls import launch_api, populate_quiz, update_score,initialize_quizzer
+
 
 class QuestionInterface(Widget):
+    #############################
+    # Variable Defines
+    # ham_menu = ObjectProperty(None)
+    # ####################################
+    # question_data = ObjectProperty(None)
+    # stats_feed = ObjectProperty(None)
+    # question_text = ObjectProperty(None)
+    # question_media = ObjectProperty(None)
+    # answer_text = ObjectProperty(None)
+    # answer_media = ObjectProperty(None)
+    # user_input = ObjectProperty(None)
+    #################################### 
+       
+            
     def __init__(self, **kwargs):
         super(QuestionInterface, self).__init__(**kwargs)
+        # Main Script
+        # Populate a quiz upon launch
+        self.question_list, self.returned_sorted_questions = populate_quiz()
+        # Display the first question
+        self.display_question()
+        self.has_seen_answer = False
+        # Startup complete
         
+    def make_quiz_if_empty(self):
+        if len(self.question_list) <= 0:
+            self.question_list, self.returned_sorted_questions = populate_quiz() 
+        
+    def display_question(self):
+        '''
+        Displays a question and returns data about that question
+        First Phase of the Loop
+        '''
+        self.has_seen_answer = False
+        self.make_quiz_if_empty()
+        self.current_question = self.question_list[0]
+        if self.current_question.get("file_name") != None:
+            self.file_name = self.current_question.get("file_name")
+            
+        if self.current_question.get("file_path") != None:
+            self.file_path = self.current_question.get("file_path")
+            
+        if self.current_question.get("type") != None:
+            self.type = self.current_question.get("type")
+            
+        if self.current_question.get("subject") != None:
+            self.subject = self.current_question.get("subject")
+            label_string = f'Subject(s): ' + " & ".join(self.subject)
+            self.ids.question_data.text = label_string
+            
+        if self.current_question.get("related") != None:
+            self.related = self.current_question.get("related")
+        
+        
+        if self.current_question.get("question_text") != None:
+            self.question_text = self.current_question.get("question_text")
+            self.ids.question_text.text = self.question_text
+            
+        if self.current_question.get("question_media") != None:
+            self.question_media = self.current_question.get("question_media")
+            # Load media
+        else:
+            self.ids.question_media.text = ""
+            
+        if self.current_question.get("answer_media") != None:
+            self.answer_media = self.current_question.get("answer_media")
+            
+        if self.current_question.get("answer_text") != None:
+            self.answer_text = self.current_question.get("answer_text")
+            
+        if self.current_question.get("next_revision_due") != None:
+            self.next_revision_due = self.current_question.get("next_revision_due")
+            
+        if self.current_question.get("revision_streak") != None:    
+            self.revision_streak = self.current_question.get("revision_streak")
+            
+        if self.current_question.get("last_revised") != None:
+            self.last_revised = self.current_question.get("last_revised")
+        
+        # Fill in stats_feed section
+        label_string = f"{'Remaining Questions':<25}: {str(len(self.question_list))} \n {'Questions for Review':<25}: {str(len(self.returned_sorted_questions))}"
+        self.ids.stats_feed.text = label_string
+        return None
+    
+    def show_answer(self):
+        '''
+        When button pressed, function shows the answer and enables the use of the scoring buttons
+        Second Phase of the Loop
+        '''
+        self.has_seen_answer = True
+        self.ids.answer_text.text = self.answer_text
+        if self.answer_media != None:
+            pass # Need to look up image display to get this portion to work #FIXME
+    def question_correct(self):
+        if self.has_seen_answer == True:
+            self.question_list.pop(0)
+            self.clear_fields()
+            self.display_question()
+            
+        else:
+            print("Button Disabled")
+        
+    def question_incorrect(self):
+        if self.has_seen_answer == True:
+            self.question_list.pop(0)
+            self.clear_fields()
+            self.display_question()
+            
+        else:
+            print("Button Disabled")
+    def question_skip(self):
+        self.question_list.pop(0)
+        self.clear_fields()
+        self.display_question()
+        pass
+    def clear_fields(self):
+        self.ids.answer_text.text = ""
+        self.ids.answer_media.text = "" #FIXME
+        self.ids.question_text.text = ""
+        self.ids.question_media.text = "" #FIXME
+        self.ids.question_data.text = ""
+        self.ids.stats_feed.text = ""
+    
 class Quizzer(App):
     def build(self):
         return QuestionInterface()
 
 if __name__ == '__main__':
-    Quizzer().run()
-
-      
-
-
-
-
-
-# class LoginScreen(GridLayout):
-#     def __init__(self, **kwargs):
-#         super(LoginScreen, self).__init__(**kwargs)
-#         self.cols = 2
-#         self.add_widget(Label(text='User Name'))
-#         self.username = TextInput(multiline=False)
-#         self.add_widget(self.username)
-#         self.add_widget(Label(text='password'))
-#         self.password = TextInput(password=True, multiline=False)
-#         self.add_widget(self.password)
+    # launch_api()
+    initialize_quizzer()
+    Quizzer().run()    
 
 
